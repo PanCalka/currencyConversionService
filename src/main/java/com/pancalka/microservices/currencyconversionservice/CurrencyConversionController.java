8b4ff052dfe3a3778dcf62ed8a5bb37cb6ef057f
panCalka <r.j.r.wojcik@gmail.com>
2018-06-01 13:15:34 +0200

    add feign client to communicate between microservices

--- src/main/java/com/pancalka/microservices/currencyconversionservice/CurrencyConversionController.java
+++ src/main/java/com/pancalka/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -1,5 +1,6 @@
 package com.pancalka.microservices.currencyconversionservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,8 +14,12 @@ import java.util.Map;
 @RestController
 public class CurrencyConversionController {
 
+    @Autowired
+    private CurrencyExchangeServiceProxy proxy;
+
     @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversionBean conversion(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity){
+
         Map<String, String> uriVariable = new HashMap<>();
         uriVariable.put("from", from);
         uriVariable.put("to", to);
@@ -28,4 +33,13 @@ public class CurrencyConversionController {
                 quantity.multiply(response.getConversionMultiple()), response.getPort());
     }
 
+    @GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversionBean conversionWithFeign(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity){
+
+
+        CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
+
+        return new CurrencyConversionBean(response.getId(),from, to, response.getConversionMultiple(), quantity,
+                quantity.multiply(response.getConversionMultiple()), response.getPort());
+    }
 }
