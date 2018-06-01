820ba0c247cfbe6f3cac31d9e9dfe93af0f7a564
panCalka <r.j.r.wojcik@gmail.com>
2018-06-01 14:36:50 +0200

    add eureka and ribbon

--- src/main/java/com/pancalka/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/com/pancalka/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,10 +2,12 @@ package com.pancalka.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.pancalka.microservices.currencyconversionservice")
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
     public static void main(String[] args) {
